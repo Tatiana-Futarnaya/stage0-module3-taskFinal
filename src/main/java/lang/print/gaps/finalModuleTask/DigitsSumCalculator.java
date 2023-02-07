59b29872605ec841fb314afb50dd743983371184
Tatiana-Futarnaya <tannia93@mail.ru>
2023-02-07 16:24:45 +0300

    first commit

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,14 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int i=4;
+        int sum=0;
+        while (i>0){
+            int x=number%10;
+            sum=sum+x;
+            number=number/10;
+            i--;
+        }
+        System.out.println(sum);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int a,b,c;
+        a=number/100;
+        b=number/10%10;
+        c=number%10;
+        System.out.println(c*100+b*10+a);
     }
 }
